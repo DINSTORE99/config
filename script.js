477be36c75f5f3d974956ce365c64f927652eb12
DINSTORE99 <dinstoree41@gmail.com>
2026-02-27 02:01:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -68,3 +68,9 @@ function startCountdown() {
 }
 
 startCountdown();
+
+window.addEventListener("load", function() {
+  setTimeout(function() {
+    document.getElementById("loadingScreen").style.display = "none";
+  }, 2000); // 2000 = 2 detik
+});
